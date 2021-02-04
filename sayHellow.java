04d279eb7b63b9a089d3a143f25f5376251092f5
Gerald Agbonye <dj_gerry@Geralds-MacBook-Pro.local>
2021-02-04 23:55:54 +0100

    modified sayhello.java

--- sayHellow.java
+++ sayHellow.java
@@ -2,8 +2,9 @@ import java.io;
 
 public class sayHello {
 
-
 public void main(Strin[] Arg){
   SYstem.out.println("hello world");
  }
+
+private void toString() { System.out.println("blablabla");}
 }
