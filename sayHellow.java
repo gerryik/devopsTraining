55df14fb6fc563f5c16c7f53ace98830cfd4ccb3
Gerald Agbonye <dj_gerry@Geralds-MacBook-Pro.local>
2021-02-05 00:04:45 +0100

    Revert "modified sayhello.java"
    This reverts commit 04d279eb7b63b9a089d3a143f25f5376251092f5.

--- sayHellow.java
+++ sayHellow.java
@@ -2,9 +2,8 @@ import java.io;
 
 public class sayHello {
 
+
 public void main(Strin[] Arg){
   SYstem.out.println("hello world");
  }
-
-private void toString() { System.out.println("blablabla");}
 }
